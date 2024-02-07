34d6b685ecd14d0b1f872ddc0f3f8381c85c46c4
Nishadchowdhury <Nishadhj111@gmail.com>
2024-02-07 15:22:33 +0600

    more components and added message generation functionalities.

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import { Card } from "@/components/ui/card"
 import { cn } from "@/lib/utils"
 import { ArrowRight, Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react"
+import { useRouter } from "next/navigation";
 
 const tools = [
     {
@@ -42,7 +43,9 @@ const tools = [
     },
 ];
 
-function dashboardPage() {
+function DashboardPage() {
+
+    const router = useRouter()
     return (
         <div className="">
             <div className="mb-8 space-y-4">
@@ -58,7 +61,9 @@ function dashboardPage() {
             <div className="px-4 md:px-20 lg:px-32 space-y-4">
                 {
                     tools.map((tool) => (
-                        <Card key={tool.href} className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer">
+                        <Card
+                            onClick={() => router.push(tool.href)}
+                            key={tool.href} className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer">
                             <div className="flex items-center gap-x-4">
                                 <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
                                     <tool.icon className={cn("w-8 h-8", tool.color)} />
@@ -75,4 +80,4 @@ function dashboardPage() {
         </div>
     )
 }
-export default dashboardPage
+export default DashboardPage
