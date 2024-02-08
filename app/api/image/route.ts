24cd8820af52124823d1be6ece40610ff54efebb
Nishadchowdhury <Nishadhj111@gmail.com>
2024-02-08 12:16:07 +0600

    Image, video, and music generator has added successfully.

---
+++ app/api/image/route.ts
@@ -0,0 +1,62 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt, amount = 1, resolution = "512x512" } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", {
+        status: 401,
+        statusText: "Please login first",
+      });
+    }
+
+    if (!configuration.apiKey) {
+      return new NextResponse("Open ai API key not configured.", {
+        status: 500,
+      });
+    }
+
+    if (!prompt) {
+      return new NextResponse("Prompt is required.", {
+        status: 500,
+      });
+    }
+
+    if (!amount) {
+      return new NextResponse("Amount is required.", {
+        status: 500,
+      });
+    }
+
+    if (!resolution) {
+      return new NextResponse("Resolution are required.", {
+        status: 500,
+      });
+    }
+
+    const response = await openai.createImage({
+      prompt,
+      n: parseInt(amount, 10),
+      size: resolution,
+    });
+
+    return NextResponse.json(response.data.data);
+  } catch (error: any) {
+    console.log("Image error :->", error.message);
+    return new NextResponse("Internal Error", {
+      status: 500,
+      statusText: error.message,
+    });
+  }
+}
